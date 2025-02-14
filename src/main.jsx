d8de0212fd5cb9a9c6f7de3f6305ce057a7b8291
IvanSantoveniaMantecon <ivansantoveniamantecon@gmail.com>
2025-02-14 19:54:16 +0100

    Guion Finalizado

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "tachyons";
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
